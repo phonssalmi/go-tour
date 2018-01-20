514baaea23e123a104b38909fc4cb97dff2ec38f
phonssalmi <phonssalmi@gmail.com>
2018-01-20 15:57:48 +0200

    Added a basic maven project for the server, added junit and opentripplanner as dependencies.

---
+++ server/src/main/java/org/test/pckg/Whatever.java
@@ -0,0 +1,13 @@
+package org.test.pckg;
+
+import org.opentripplanner.standalone.OTPMain;
+
+public class Whatever {
+	
+	public Whatever() {
+		OTPMain test = new OTPMain(null);
+		test.
+		
+	}
+	
+}
