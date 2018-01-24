da19f182e424ef839dd3d92c7aac729954fd033c
phonssalmi <phonssalmi@gmail.com>
2018-01-24 21:46:50 +0200

    Added gtfs url

--- server/src/main/java/org/test/pckg/Whatever.java
+++ server/src/main/java/org/test/pckg/Whatever.java
@@ -1,13 +1,31 @@
 package org.test.pckg;
 
+import java.io.File;
+
+import org.opentripplanner.standalone.CommandLineParameters;
 import org.opentripplanner.standalone.OTPMain;
 
 public class Whatever {
 	
+	private static final String DATA_DIR = "../../otp-data/";
+	private static final String GTFS_MAIN_URL = "http://data.foli.fi/gtfs";
+	
+	private OTPMain otp;
+	
 	public Whatever() {
-		OTPMain test = new OTPMain(null);
-		test.
+		CommandLineParameters conf = new CommandLineParameters();
+		conf.build = new File(DATA_DIR);
+		conf.inMemory = true;
+		otp = new OTPMain(conf);
 		
+	}
+	
+	private void run() {
+		otp.run();
+	}
+	
+	public static void main(String[] args) {
+		new Whatever().run();
 	}
 	
 }
