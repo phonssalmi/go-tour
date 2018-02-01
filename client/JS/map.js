ec06d660c53a23c19cb55c886fadf6c0e70de5a9
Lukasz Tomczyk <ukaszeq96@gmail.com>
2018-02-01 18:35:33 +0200

    Added routing between markers and to choose routing type
    (pedestrian, car, bicycle)

--- client/JS/map.js
+++ client/JS/map.js
@@ -1,60 +1,94 @@
-window.onload = function(){
+var startlng = 0;
+var startlat = 0;
+var endlng = 0;
+var endlat = 0;
+var polyline = null;
+var map;
+window.onload = function () {
 	/*Map creation*/
-    var map = L.map('mapid',{
-        center:[60.4500, 22.2667],
-        zoom:14,
-        zoomControl:true,	
-		scaleControl:false,
-        minZoom:4
-    });
-	
+	map = L.map('mapid', {
+		center: [60.4500, 22.2667],
+		zoom: 14,
+		zoomControl: true,
+		scaleControl: false,
+		minZoom: 4
+	});
 	/*Initial map location*/
 	map.zoomControl.setPosition('bottomright');
-	
-    map.locate({setView:true, maxZoom:14});
+
+	map.locate({ setView: true, maxZoom: 14 });
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>'
 	}).addTo(map);
-	
+
 	/*Event listeners for the map*/
-	var startMarker = L.marker([],{draggable: true});
-	var endMarker = L.marker([],{draggable: true});
-	
-	
+	var startMarker = L.marker([], { draggable: true });
+	var endMarker = L.marker([], { draggable: true });
+
 	function onMapRightClick(e) {
 		/*Place marker*/
 		endMarker
 			.setLatLng(e.latlng)
 			.addTo(map);
 		endMarker.bindPopup(JSON.stringify(endMarker.getLatLng()));
+		endlng = e.latlng.lng;
+		endlat = e.latlng.lat;
 		/*Change form value for the end marker*/
 		document.getElementById("routeForm").elements["routeEnd"].value = JSON.stringify(e.latlng);
+		if (startlng != 0 && startlat != 0) {
+			getRoute();
+		}
 	}
 	map.on('contextmenu', onMapRightClick);
-	
+
 	function onMapClick(e) {
 		/*Place marker*/
 		startMarker
 			.setLatLng(e.latlng)
 			.addTo(map);
 		startMarker.bindPopup(JSON.stringify(startMarker.getLatLng()));
+		startlng = e.latlng.lng;
+		startlat = e.latlng.lat;
+		if (endlng != 0 && endlat != 0) {
+			getRoute();
+		}
 		/*Change form value for the start marker*/
 		document.getElementById("routeForm").elements["routeStart"].value = JSON.stringify(e.latlng);
 	}
 	map.on('click', onMapClick);
-	
+
 	function startMarkerDrag(e) {
 		document.getElementById("routeForm").elements["routeStart"].value = JSON.stringify(e.latlng)
 	}
 	startMarker.on('move', startMarkerDrag);
-	
+
 	function endMarkerDrag(e) {
 		document.getElementById("routeForm").elements["routeEnd"].value = JSON.stringify(e.latlng)
 	}
 	endMarker.on('move', endMarkerDrag);
 }
+function getRoute() {
+	var request = new XMLHttpRequest();
+	var requestURI = 'https://api.openrouteservice.org/directions?api_key=58d904a497c67e00015b45fce7820addba544082bfb751a87dd60ca8&coordinates='
+	+ startlng + '%2C' + startlat + '%7C' + endlng + '%2C' + endlat + '&profile=' + getTransportType();
+	request.open('GET', requestURI);
+	request.setRequestHeader('Accept', 'text/json; charset=utf-8');
 
+	request.onreadystatechange = function () {
+		if (this.readyState === 4) {
+			var encoded = JSON.parse(this.response).routes[0].geometry;
+			console.log(this.responseText);
+			if (polyline != null) {
+				map.removeLayer(polyline);
+			}
+			polyline = L.Polyline.fromEncoded(encoded).addTo(map);
 
+		}
+	};
 
-
-
+	request.send();
+}
+function getTransportType() {
+    var x = document.getElementById("transportType").value;
+	return x;
+}
