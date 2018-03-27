4595630caef432d72467f6546e5baf3f012b497c
amaczkowski <arkadiusz.maczkowski@gmail.com>
2018-03-27 11:05:16 +0300

    Deleting cache after reloading the page

--- client/JS/map.js
+++ client/JS/map.js
@@ -21,6 +21,7 @@ var inputStringHTML = "<div class=\"leaflet-routing-geocoder\">" +
 var orsKey = "58d904a497c67e00015b45fce7820addba544082bfb751a87dd60ca8";
 
 window.onload = function () {
+
 	/*Map creation*/
 	map = L.map('mapid', {
 		center: [60.4500, 22.2667],
@@ -29,7 +30,7 @@ window.onload = function () {
 		scaleControl: false,
 		minZoom: 4
 	});
-
+	recreateInputs();
 	document.getElementById("car").style.backgroundColor = "blue";
 
 	inputsArray = document.getElementsByClassName("input-fields");
@@ -471,10 +472,10 @@ function onRangeChange() {
 	interval.value = Math.max(interval.min, interval.value);
 	document.getElementById("rangeMin").innerHTML = "Min:" + range.min;
 	document.getElementById("rangeMax").innerHTML = "Max:" + range.max;
-	document.getElementById("rangeCurrent").innerHTML = range.value + "min";
+	document.getElementById("rangeCurrent").innerHTML = range.value + " min";
 
 	document.getElementById("intervalMin").innerHTML = "Min:" + interval.min;
-	document.getElementById("intervalCurrent").innerHTML = interval.value + "min";
+	document.getElementById("intervalCurrent").innerHTML = interval.value + " min";
 }
 
 function placeMarkers() {
